e964220a442419668ed3ecae2b4492075fee8680
Darrius Davidson <ddav218@lsu.edu>
2024-10-29 15:37:50 -0500

    ReflectifyWebDev

--- ReflectifyWebDev/src/App.tsx
+++ ReflectifyWebDev/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; 
+//import {BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; 
 import ReflectifyLogo from './my-app-front-dev/FrontDev/components/assets/ReflectifyLogo.png';
 import './App.css';
 import HomePage from './my-app-front-dev/FrontDev/components/pages/HomePage/homepage.tsx';
-import PersonalJournal from './my-app-front-dev/FrontDev/components/pages/PersonalJournal/perjournal.tsx';
+//import PersonalJournal from './my-app-front-dev/FrontDev/components/pages/PersonalJournal/perjournal.tsx';
 
 function App() {
   return (
