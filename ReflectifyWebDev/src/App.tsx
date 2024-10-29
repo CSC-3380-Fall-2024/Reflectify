f3e079c74b4cbd5e7849cc470171d17923f11f1f
Darrius Davidson <ddav218@lsu.edu>
2024-10-29 16:02:46 -0500

    Home page dev (#4)
    * HomePage & PersonalJournal updates
    
    * Implementation of HomePage into App
    
    * HomePageDev
    
    * ReflectifyWebDev
    
    * Settings.py from backend

---
+++ ReflectifyWebDev/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+//import {BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; 
+import ReflectifyLogo from './my-app-front-dev/FrontDev/components/assets/ReflectifyLogo.png';
+import './App.css';
+import HomePage from './my-app-front-dev/FrontDev/components/pages/HomePage/homepage.tsx';
+//import PersonalJournal from './my-app-front-dev/FrontDev/components/pages/PersonalJournal/perjournal.tsx';
+
+function App() {
+  return (
+      <div className="App">
+          <img src={ReflectifyLogo} alt="logo" />
+          <HomePage />
+      </div>
+  );
+}
+
+export default App;
