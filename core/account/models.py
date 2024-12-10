9e8041fb978bf2310aafca82d7cf350659f97407
BlackMoonCoder <110649898+BlackMoonCoder@users.noreply.github.com>
2024-12-09 21:38:59 -0600

    ModelsQuickFix

--- core/account/models.py
+++ core/account/models.py
@@ -4,19 +4,16 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **kwargs):
     def create_user(self, username, email, password, **kwargs):
         if username is None:
             raise TypeError ('Users must have a username!')
         if email is None:
             raise TypeError ('Users must have an email!')
-        user = self.model(username = username, email=self.normalize_email(email))
         user = self.model(username = username, email=self.normalize_email(email), **kwargs)
         user.set_password(password)
         user.save(using=self._db)
         return user
     def create_superuser (self, username, email, password):
-    def create_superuser (self, username, email, password=None):
         if password is None:
             raise TypeError ('Admins must have a password!')
         if email is None:
@@ -30,11 +27,9 @@ class UserManager(BaseUserManager):
         return user
 class User(AbstractBaseUser, PermissionsMixin):
         username = models.CharField(db_index=True, max_length=255, unique=True)
-        email = models.EmailField(db_index=True, unique=True)
+        email = models.EmailField(db_index=True, unique=True, default = 'no-reply@example.com')
         is_active = models.BooleanField(default=True)
         is_staff = models.BooleanField(default=False)
-        USERNAME_FIELD = 'email'
-        REQUIRED_FIELDS = ['username']
         USERNAME_FIELD = 'username'
         REQUIRED_FIELDS = []
         objects = UserManager()
